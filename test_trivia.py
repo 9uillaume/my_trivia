1ac3a26cdd82ca4e2c95bb809188a4c6aeec8920
Guiz <guiz.briard@gmail.com>
2018-02-10 19:59:04 +0100

    TEST content rock questions

--- test_trivia.py
+++ test_trivia.py
@@ -2,10 +2,18 @@
 from trivia import Game
 from random import randrange
 
-game = Game()
 
+# should be 50
 def test_size_of_questions():
-    assert len(game.pop_questions) == 50
-    assert len(game.science_questions) == 50
-    assert len(game.sports_questions) == 50
-    assert len(game.rock_questions) == 50
+    test_game = Game()
+    assert len(test_game.pop_questions) == 50
+    assert len(test_game.science_questions) == 50
+    assert len(test_game.sports_questions) == 50
+    assert len(test_game.rock_questions) == 50
+
+
+# should be "Rock Question {index}"
+def test_rock_questions_content():
+    test_game = Game()
+    for q in range(50):
+        assert test_game.rock_questions[q] == "Rock Question %s" % q
